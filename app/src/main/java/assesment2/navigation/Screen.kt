0cdadfd2f471f46bd1eb0382331f96fbdff96341
muflih <muflihsyarif44@gmail.com>
2025-05-03 17:24:23 +0700

    Membuat tampilan form catatan

--- app/src/main/java/assesment2/navigation/Screen.kt
+++ app/src/main/java/assesment2/navigation/Screen.kt
@@ -2,4 +2,5 @@ package assesment2.navigation
 
 sealed class Screen (val route: String){
     data object Home: Screen("mainScreen")
+    data object FormBaru: Screen("detailScreen")
 }
--- app/src/main/java/assesment2/ui/screen/DetailScreen.kt
+++ app/src/main/java/assesment2/ui/screen/DetailScreen.kt
@@ -1,2 +1,103 @@
 package assesment2.ui.screen
 
+import android.content.res.Configuration
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardCapitalization
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import assesment2.ui.theme.Assesment2Theme
+import com.muflihsyarif0023.assesment2.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DetailScreen(){
+    var judul by remember { mutableStateOf("") }
+    var mimpi by remember { mutableStateOf("") }
+
+    Scaffold (
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = stringResource(id = R.string.tambah_mimpi))
+                },
+                colors = TopAppBarDefaults.mediumTopAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary
+                )
+            )
+        }
+    ){ padding ->
+        FormMimpi(
+            title = judul,
+            onTitlechange = {judul = it},
+            desc = mimpi,
+            onDescChange = { mimpi = it },
+            modifier = Modifier.padding(padding)
+        )
+
+    }
+}
+
+@Composable
+fun FormMimpi(
+    title: String, onTitlechange: (String) -> Unit,
+    desc: String, onDescChange: (String) -> Unit,
+    modifier: Modifier
+){
+    Column (
+        modifier = modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ){
+        OutlinedTextField(
+            value = title,
+            onValueChange = { onTitlechange(it) },
+            label = { Text(text = stringResource(R.string.judul)) },
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(
+                capitalization = KeyboardCapitalization.Words,
+                imeAction = ImeAction.Next
+            ),
+            modifier = Modifier.fillMaxWidth()
+        )
+        OutlinedTextField(
+            value = desc,
+            onValueChange = { onDescChange(it) },
+            label = { Text(text = stringResource(R.string.isi_mimpi)) },
+            keyboardOptions = KeyboardOptions(
+                capitalization = KeyboardCapitalization.Sentences,
+            ),
+            modifier = Modifier.fillMaxWidth()
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
+@Composable
+fun DetailScreenPreview(){
+    Assesment2Theme {
+        DetailScreen()
+    }
+}
