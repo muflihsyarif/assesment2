4e87abb1b0cd43906bf0e9b9ad15ca44661b4509
muflih <muflihsyarif44@gmail.com>
2025-05-03 16:23:15 +0700

    3.2. Membuat tampilan list

--- app/src/main/java/assesment2/ui/screen/MainScreen.kt
+++ app/src/main/java/assesment2/ui/screen/MainScreen.kt
@@ -1,6 +1,14 @@
 package assesment2.ui.screen
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -10,6 +18,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import assesment2.model.Mimpi
 import com.muflihsyarif0023.assesment2.R
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -36,5 +49,37 @@ fun MainScreen(){
 
 @Composable
 fun ScreenContent(modifier: Modifier){
+    val viewModel: MainViewModel = viewModel()
+    val data = viewModel.data
 
+    LazyColumn (
+        modifier = modifier.fillMaxSize()
+    ){
+        items (data){
+            ListItem(mimpi = it)
+            HorizontalDivider()
+        }
+    }
+}
+
+@Composable
+fun ListItem(mimpi: Mimpi){
+    Column (
+        modifier = Modifier.fillMaxWidth().padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ){
+        Text(
+            text = mimpi.judul,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis,
+            fontWeight = FontWeight.Bold
+        )
+        Text(
+            text = mimpi.mimpi,
+            maxLines = 2,
+            overflow = TextOverflow.Ellipsis
+            )
+        Text(text = mimpi.suasana)
+        Text(text = mimpi.tanggal)
+    }
 }
